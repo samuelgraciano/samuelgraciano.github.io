66c142f0f469ad990642e7b73529efef9bb11ebd
duvan grisales <grisales_08@hotmail.es>
2017-11-17 12:32:18 -0500

    he agregado la funcion de datos personales

--- js/javascript.js
+++ js/javascript.js
@@ -201,5 +201,115 @@
 		document.getElementById("botonPago").disabled = false;
 	}	
 
+		
+//FUNCION DE VALIDACION DE DATOS PERSONALES:
+	function validaDatosPersonales(elEvento) {
+
+		var todoBien = true;
+	
+	
+		 //Nombre:
+			var vNombre = document.getElementById("nombre").value;
+			if( vNombre == null || vNombre.length == 0 || /^\s+$/.test(vNombre) || !isNaN(vNombre)) {
+				todoBien=false;
+				document.getElementById("nombre").className = "textMal";
+			}
+			else{
+				document.getElementById("nombre").className = "textBien";
+			}	
+	
+	
+		//cedula:	
+			var cedula = document.getElementById("cedula").value;
+			if( !(/^\d{10}$/.test(cedula))  ) {
+				todoBien=false;
+				document.getElementById("cedula").className = "textMal";
+			}
+			else{
+				document.getElementById("cedula").className = "textBien";
+			}	
+	
+	
+	
+	
+		//Fecha de nacimiento DIA:
+			var vFechaNacimientoDia = document.getElementById("fechaNacimientoDia").selectedIndex;
+			if( vFechaNacimientoDia == null || vFechaNacimientoDia == 0 ) {
+				todoBien=false;
+				document.getElementById("fechaNacimientoDia").className = "textMal";
+			}
+			else{
+				document.getElementById("fechaNacimientoDia").className = "textBien";
+			}
+		//Fecha de nacimiento MES:
+			var vFechaNacimientoMes = document.getElementById("fechaNacimientoMes").selectedIndex;
+			if( vFechaNacimientoMes == null || vFechaNacimientoMes == 0 ) {
+				todoBien=false;
+				document.getElementById("fechaNacimientoMes").className = "textMal";
+			}
+			else{
+				document.getElementById("fechaNacimientoMes").className = "textBien";
+			}	
+		//Fecha de nacimiento AÑO:
+			var vFechaNacimientoAnio = document.getElementById("fechaNacimientoAnio").selectedIndex;
+			if( vFechaNacimientoAnio == null || vFechaNacimientoAnio == 0 ) {
+				todoBien=false;
+				document.getElementById("fechaNacimientoAnio").className = "textMal";
+			}
+			else{
+				document.getElementById("fechaNacimientoAnio").className = "textBien";
+			}	
+	
+	
+		//Telefono:
+			var vMovil = document.getElementById("movil").value;
+			if( !(/^\d{9}$/.test(vMovil))  ) {
+				todoBien=false;
+				document.getElementById("movil").className = "textMal";
+			}
+			else{
+				document.getElementById("movil").className = "textBien";
+			}	
+	
+	
+		//email:
+			var vEmail1 = document.getElementById("email1").value;
+			var vEmail2 = document.getElementById("email2").value;
+
+			//email 1
+			if( !(/^\w+([-.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(vEmail1)) ) {
+				todoBien=false;
+				document.getElementById("email1").className = "textMal";
+			}
+			else{
+				document.getElementById("email1").className = "textBien";
+			}
+			
+			//email 2
+			if( !(/^\w+([-.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(vEmail2)) ) {
+				todoBien=false;
+				document.getElementById("email2").className = "textMal";
+			}
+			else{
+				document.getElementById("email2").className = "textBien";
+			}
+
+			//Comparacion email 1 y 2
+			if (vEmail1 != vEmail2){
+				todoBien=false;
+				document.getElementById("email2").className = "textMal";
+			}
+
+		
+	
+		//Si no ha habido ni un solo error, se ejecuta la siguiente funcion que se encarga de mostrar el formulario de los datos personales:
+		if (todoBien){
+			pideDatosPago();
+		}
+		else{
+			document.getElementById("botonConfirmar").disabled = true;
+		}
+	}
+
 	
 	
