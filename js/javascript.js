d4063105bfdbc7e5bdee1139142f19e34993dfc0
duvan grisales <grisales_08@hotmail.es>
2017-11-17 12:19:13 -0500

    he agregado la carpeta js

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+﻿//	Hacer tienda online de informatica usando: HTML, CSS, JS
+//	En el codigo javascript hay que hacer la base de datos de los productos con un vector por ejemplo...
+
+
+
+
+//BASE DE DATOS
+	var productos = ["Bolsas de drenaje", "Bolsa2000ml", "Bolsa de pierda", "NEBULIZADOR", "PULSO OXIMETRO", "Producto Agotado", "Producto agotado", "Producto agotado", "Producto agotado"];
+	var imgGrandes = ["imagess/producto1.jpg", "imagess/producto2.jpg", "img/productos/3.jpg", "imagess/producto3.jpg", "imagess/producto4.jpg", "imagess/producto5.jpg", "img/productos/7.jpg", "img/productos/8.jpg", "img/productos/9.jpg"];
+	var imgPeque = ["img/productos/1m.jpg", "img/productos/2m.jpg", "img/productos/3m.jpg", "img/productos/4m.jpg", "img/productos/5m.jpg", "img/productos/6m.jpg", "img/productos/7m.jpg", "img/productos/8m.jpg", "img/productos/9m.jpg"];
+	var precios = [33000, 169000, 360000, 36000, 11000, 540000, 21000, 66000, 25000];
+	
+	
+	var stock = [5, 2, 8, 3, 10, 0, 0, 0, 0];
+	var precioTransporte = [6000, 12000, 20000, "gratis"];
+	var IVA = 0.19;
+	var uniUser;
+	
+	
+
+	
+	
+	
